ecc8aaa2d5f0860e7f60171bc09dc864c6b34dd9
Muhammad Hasbi <mh217512@gmail.com>
2025-04-03 17:29:10 +0700

    change popup notification

--- main.py
+++ main.py
@@ -1,19 +1,24 @@
 import time
-import plyer
+import tkinter as tk
+from tkinter import messagebox
+import winsound
 
-def send_notification(title, message):
-    plyer.notification.notify(
-        title=title,
-        message=message,
-        timeout=10
-    )
+
+def show_popup(title, message):
+    root=tk.Tk()
+    root.withdraw()
+    messagebox.showinfo(title, message)
+
+def play_sound():
+    winsound.Beep(1000, 500)
 
 def pomodoro_timer(study_time=25, break_time=5):
     while True:
-        send_notification("Watunya belajar", "Fokus selama 25 menit!")
+
+        show_popup("Watunya belajar", "Fokus selama 25 menit!")
         time.sleep(study_time*60)
 
-        send_notification("Waktunya istirahat", "Istirahat selama 5 menit!")
+        show_popup("Waktunya istirahat", "Istirahat selama 5 menit!")
         time.sleep(break_time*60)
 
 if __name__ == "__main__":
